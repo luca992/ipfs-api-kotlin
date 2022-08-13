e42d9b767cd080da502b1eb33a65f1be1a3e19ad
Luca Spinazzola <mspinluca@gmail.com>
2022-08-13 19:02:28 -0400

    update ktor and com.vanniktech.maven.publish

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
         kotlin("jvm") version "1.7.10"
         kotlin("plugin.serialization") version "1.7.10"
         id("com.github.ben-manes.versions") version "0.42.0"
-        id("com.vanniktech.maven.publish") version "0.19.0"
+        id("com.vanniktech.maven.publish") version "0.21.0"
     }
 }
 
